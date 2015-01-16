b1de39ee12c4cc4c1e4424ca758b6b617a107a29
michaely <gt8442b@yahoo.com>
2015-01-16 08:58:20 -0500

    Update create_mysql_db_user.sh

--- create_mysql_db_user.sh
+++ create_mysql_db_user.sh
@@ -8,3 +8,6 @@ mysql -uroot -p$MYSQL_ROOT_PW -e "GRANT ALL PRIVILEGES ON $MYSQL_WYUN_DB.* TO '$
 mysql -uroot -p$MYSQL_ROOT_PW -e "GRANT ALL PRIVILEGES ON $MYSQL_WYUN_DB.* TO '$MYSQL_WYUN_USER'@'%' IDENTIFIED BY '$MYSQL_WYUN_PW';"
 mysql -uroot -p$MYSQL_ROOT_PW -e "GRANT ALL PRIVILEGES ON $MYSQL_WYUN_DB.* TO '$MYSQL_WYUN_USER'@'0.0.0.0' IDENTIFIED BY '$MYSQL_WYUN_PW';"
 mysql -uroot -p$MYSQL_ROOT_PW -e "GRANT ALL PRIVILEGES ON $MYSQL_WYUN_DB.* TO '$MYSQL_WYUN_USER'@'127.0.0.1' IDENTIFIED BY '$MYSQL_WYUN_PW';"
+
+#To view database with utf8 characters
+mysql -u user -p0ffs4t? wlsp --default-character-set=utf8
